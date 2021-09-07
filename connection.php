15f51d83ed3ddce14e636289971b71b912ef8032
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-09-07 02:35:27 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "itgo";
+$host = "us-cdbr-east-03.cleardb.com";
+$user = "b008e38dc3d446";
+$pass = "98f53217";
+$db = "heroku_de779d0e3559015";
 
 $connect = mysqli_connect($host, $user, $pass, $db);
 ?>
