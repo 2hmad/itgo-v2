268d4802395dfec02170778c768cb0e9202960e7
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-09-10 15:59:13 +0200

    update

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "us-cdbr-east-03.cleardb.com";
-$user = "b008e38dc3d446";
-$pass = "98f53217";
-$db = "heroku_de779d0e3559015";
+$host = "app-4eb29582-56b6-4716-897a-25df8089b678-do-user-9367059-0.b.db.ondigitalocean.com";
+$user = "itgo";
+$pass = "vlgwQLOv0k2Ykq2d";
+$db = "itgo";
 
 $connect = mysqli_connect($host, $user, $pass, $db);
 $connect -> set_charset("utf8");
---
+++ heroku_de779d0e3559015_clients.sql
@@ -0,0 +1,4 @@
+insert into heroku_de779d0e3559015.clients (id, name, logo)
+values  (1, 'مدارس جيل الريادة الاهلية', '3.jpg'),
+        (2, 'Sary Academy - ساري اكاديمي', '1.png'),
+        (3, 'ينابيع - Yanabee', '2.jpg');
