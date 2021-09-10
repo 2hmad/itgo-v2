5e2e08d11bc496e86335c6da91e47900e67a22fb
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-09-10 15:11:37 +0200

    Update work.php

--- work.php
+++ work.php
@@ -44,11 +44,11 @@ if (isset($_GET['id'])) {
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@400;700;800&display=swap" rel="stylesheet"> <!-- <link rel="stylesheet" type="text/css" href="css/slick-theme.css" /> -->
     <link rel="stylesheet" href="css/slick.css">
     <?php 
-    if(mb_strlen($details, 'UTF-8') <= 160) {
-        echo '<meta name="description" content="'.$details.'">';
-    } else {
-        echo '<meta name="description" content="'.mb_substr($details, 0, 160, 'UTF-8').'">';
-    }    
+    // if(mb_strlen($details, 'UTF-8') <= 160) {
+    //     echo '<meta name="description" content="'.$details.'">';
+    // } else {
+    //     echo '<meta name="description" content="'.mb_substr($details, 0, 160, 'UTF-8').'">';
+    // }    
     ?>
     <meta name="robots" content="index, follow">
 
