5521c83c95b246719c9a02c5a9ee57416e84f8eb
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2021-09-12 16:38:01 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-$host = "app-4eb29582-56b6-4716-897a-25df8089b678-do-user-9367059-0.b.db.ondigitalocean.com";
-$user = "itgo";
-$pass = "vlgwQLOv0k2Ykq2d";
-$db = "itgo";
-
-$connect = mysqli_connect($host, $user, $pass, $db);
-$connect -> set_charset("utf8");
-
-=======
 <?php
 
 $host = "us-cdbr-east-03.cleardb.com";
@@ -20,5 +8,4 @@ $db = "heroku_de779d0e3559015";
 $connect = mysqli_connect($host, $user, $pass, $db);
 $connect -> set_charset("utf8");
 
->>>>>>> 3ae9ab182c0fe124d0e54c23212650e2f1ca0a65
 ?>
