a0dd56a0a74025b738757e5895f71597df0e300a
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2021-09-18 23:21:38 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "us-cdbr-east-03.cleardb.com";
-$user = "b008e38dc3d446";
-$pass = "98f53217";
-$db = "heroku_de779d0e3559015";
+$host = "us-cdbr-east-04.cleardb.com";
+$user = "b55db4e4498a14";
+$pass = "1e181fff";
+$db = "heroku_eee95f88829cde5";
 
 $connect = mysqli_connect($host, $user, $pass, $db);
 $connect -> set_charset("utf8");
